872c24d615f554f160ec46616945c4e7781fa715
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-05-14 11:28:10 -0400

    all new commit

--- TEST/selenium/InptBoxTest.java
+++ TEST/selenium/InptBoxTest.java
@@ -18,7 +18,21 @@ public class InptBoxTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		System.setProperty("webdriver.chrome.driver","/Users/macstudent/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		//1. tell selenium what page to test
+		driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+//2. type your msg into text box(id=user message )(send keys ("....."))
+		textBox.sendkeys("hello world");
+		//3. Find the button Form#get-input button
+		WebElement button=driver.findElement(By.cssSelector("form#get-input buttton"));
+		//4. click on the button
+		button.click();
+		//5. find the output msg thing(id=msg)
+		webElement outputSpan=driver.findElement9By.id("displey"));
+		
+		webElement button=driver.findElement(By.cssSelector("for#get-input button"));
+		
 	}
 
 }
